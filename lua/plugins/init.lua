6b321f512fbc6e3c71e6dc06ea2468b6b9b7661c
Vinícius Lino Figueirêdo <vinicius.figueiredo@fcxlabs.com.br>
2025-02-26 09:46:11 -0300

    ➕ build: Adicionando dependência do ToggleTerm
    Adicionando terminal de sobreposição ao nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,2 +1,13 @@
 return {
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = function()
+			require("toggleterm").setup({
+				open_mapping = [[<C-]>]], -- Atalho para abrir/fechar o terminal
+				direction = "float", -- Pode ser "horizontal", "vertical", "float" ou "tab"
+				shade_terminals = true, -- Deixa o terminal com um tom mais escuro
+			})
+		end,
+	},
 }
