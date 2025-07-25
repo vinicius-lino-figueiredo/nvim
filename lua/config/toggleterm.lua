f730360fab6eac3db2e85a0333f059d773e173eb
Vinícius Lino Figueirêdo <vinicius.figueiredo@fcxlabs.com.br>
2025-07-25 13:55:09 -0300

    🔧 chore: Reconfigura terminal por tab
    Configura plugin Toggleterm para abrir terminal no diretório local (lcd).

--- lua/config/toggleterm.lua
+++ lua/config/toggleterm.lua
@@ -9,4 +9,27 @@ toggleterm.setup({
 	open_mapping = open_mapping,
 	direction = "float",
 	shade_terminals = true,
+	persist_mode = false,
 })
+
+local Terminal = require("toggleterm.terminal").Terminal
+
+function _G.get_tab_terminal()
+	local tab_id = vim.api.nvim_get_current_tabpage()
+	local term = Terminal:new({
+		count = tab_id,
+		direction = "float",
+		on_open = function(term)
+			vim.cmd("startinsert!")
+		end,
+	})
+	return term
+end
+
+function _G.toggle_tab_terminal()
+	local term = get_tab_terminal()
+	term:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<C-]>", "<cmd>lua toggle_tab_terminal()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<C-]>", "<cmd>lua toggle_tab_terminal()<CR>", { noremap = true, silent = true })
