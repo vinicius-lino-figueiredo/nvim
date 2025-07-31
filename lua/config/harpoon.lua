51129e7a835f9f2044547c80d2ae505bf7bfb106
Vinícius Lino Figueirêdo <vinicius.figueiredo@fcxlabs.com.br>
2025-07-31 13:50:42 -0300

    🔧 chore: Reconfigura atalho para troca de buffer no harpoon
    Reconfigura atalho de troca de buffer do harpoon para <leader>p e <leader>n

--- lua/config/harpoon.lua
+++ lua/config/harpoon.lua
@@ -13,5 +13,5 @@ vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
 vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+vim.keymap.set("n", "<leader>p", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<leader>n", function() harpoon:list():next() end)
