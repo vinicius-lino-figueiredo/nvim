67a409f902b3bbf295b4b19f82b8ab3e931095de
Vinícius Lino Figueirêdo <vinicius.figueiredo@fcxlabs.com.br>
2025-08-18 09:36:55 -0300

    🐛 fix: Ajustando erro do dap ui
    Resolvendo erro ao inicializar paineis do dap ui

--- lua/config/dap.lua
+++ lua/config/dap.lua
@@ -118,8 +118,17 @@ vim.keymap.set("n", "<Leader>X", function()
 	require("dap").terminate()
 end)
 vim.keymap.set("n", "<Leader>w", function()
-	dapui.open()
+	local ok, err = pcall(dapui.open)
+	if not ok then
+		print("Error opening DAP UI: " .. err)
+		-- Try to reinitialize
+		dapui.setup()
+		dapui.open()
+	end
 end)
 vim.keymap.set("n", "<Leader>W", function()
-	dapui.close()
+	local ok, err = pcall(dapui.close)
+	if not ok then
+		print("Error closing DAP UI: " .. err)
+	end
 end)
