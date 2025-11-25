bc0c6208c6c45ca29129e247600aaed0947902b3
Vinícius Lino Figueirêdo <vinicius.figueiredo@fcxlabs.com.br>
2025-11-25 14:45:47 -0300

    feat(lsp): adiciona e configura lsp e linter odin

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -33,4 +33,28 @@ return {
 	clangd = {},
 	lemminx = {},
 	bashls = {},
+	ols = {
+		init_options = {
+			checker_args = "-strict-style",
+		},
+		on_attach = function(client, bufnr)
+			-- Força o checker a rodar ao abrir o arquivo
+			vim.defer_fn(function()
+				client:notify("textDocument/didSave", {
+					textDocument = { uri = vim.uri_from_bufnr(bufnr) },
+				})
+			end, 100)
+		end,
+		before_init = function(_, config)
+			local ols_json = config.root_dir .. "/ols.json"
+			if vim.fn.filereadable(ols_json) == 0 then
+				local content = [[{
+  "$schema": "https://raw.githubusercontent.com/DanielGavin/ols/master/misc/ols.schema.json",
+  "profile": "default",
+  "profiles": [{ "name": "default", "checker_path": ["."] }]
+}]]
+				vim.fn.writefile(vim.split(content, "\n"), ols_json)
+			end
+		end,
+	},
 }
