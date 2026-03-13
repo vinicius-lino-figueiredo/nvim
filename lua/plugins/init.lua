f26e777d2a9267477c5c3d2be335bb88f7a9877d
Vinícius Lino Figueirêdo <viniciusfigueiredo725@protonmail.com>
2026-03-13 16:00:17 -0300

    chore: atualiza catppuccin e ajusta breaking change

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -20,7 +20,7 @@ return {
 				flavour = "mocha", -- latte, frappe, macchiato, mocha
 			})
 			-- setup must be called before loading
-			vim.cmd.colorscheme("catppuccin")
+			vim.cmd.colorscheme("catppuccin-nvim")
 		end,
 	},
 	{ "hrsh7th/nvim-cmp" },
