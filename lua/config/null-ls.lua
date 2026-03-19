9527961502e098aad45c8570fbae4502deaa03b2
Vinícius Lino Figueirêdo <viniciusfigueiredo725@protonmail.com>
2026-03-19 12:47:48 -0300

    refactor: usa golangci-lint em vez de revive para diagnósticos

--- lua/config/null-ls.lua
+++ lua/config/null-ls.lua
@@ -4,7 +4,11 @@ null_ls.setup({
     sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.completion.spell,
-        null_ls.builtins.diagnostics.revive,
+        null_ls.builtins.diagnostics.golangci_lint,
         null_ls.builtins.formatting.xmllint,
     },
+})
+
+null_ls.builtins.diagnostics.golangci_lint.with({
+  extra_args = { "--config", ".golangci.yml" },
 })
