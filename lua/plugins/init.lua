cfb5f3f6de91e36c5baf9780366070cd7cd4897a
Vinícius Lino Figueirêdo <viniciusfigueiredo725@protonmail.com>
2026-03-19 12:56:54 -0300

    revert: remove plugin específico do go
    O set já configurado de lsp, dap, diagnostics e outros já basta para
    suportar o go.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -29,20 +29,7 @@ return {
 	{ "hrsh7th/cmp-path" },
 	{ "hrsh7th/cmp-vsnip" },
 	{ "hrsh7th/vim-vsnip" },
-	{
-		"ray-x/go.nvim",
-		dependencies = { -- optional packages
-			"ray-x/guihua.lua",
-			"neovim/nvim-lspconfig",
-			"nvim-treesitter/nvim-treesitter",
-		},
-		config = function()
-			require("go").setup()
-		end,
-		event = { "CmdlineEnter" },
-		ft = { "go", "gomod" },
-		build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
-	},
+	{ "neovim/nvim-lspconfig" },
 	{
 		"L3MON4D3/LuaSnip",
 		-- follow latest release.
