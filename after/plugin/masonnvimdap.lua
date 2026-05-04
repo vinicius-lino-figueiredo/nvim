7cff17f7b7665c55285c11628a755a2087b344e1
Vinícius Lino Figueirêdo <viniciusfigueiredo725@protonmail.com>
2026-05-04 15:20:58 -0300

    fix(Mason/Go/DAP): Seta delve como remote para ajustar output

--- after/plugin/masonnvimdap.lua
+++ after/plugin/masonnvimdap.lua
@@ -1,4 +1,63 @@
 require("mason-nvim-dap").setup({
 	automatic_installation = false,
-	handlers = {},
+	handlers = {
+		delve = function(config)
+			config.adapters = {
+				type = "server",
+				port = "${port}",
+				executable = {
+					command = vim.fn.stdpath("data") .. "/mason/bin/dlv",
+					args = { "dap", "-l", "127.0.0.1:${port}" },
+				},
+			}
+			config.configurations = {
+				{
+					type = "delve",
+					name = "Debug Package",
+					request = "launch",
+					program = "./${relativeFileDirname}",
+					outputMode = "remote",
+				},
+				{
+					type = "delve",
+					name = "Debug Package (tests)",
+					request = "launch",
+					mode = "test",
+					program = "./${relativeFileDirname}",
+					outputMode = "remote",
+				},
+				{
+					type = "delve",
+					name = "Debug Package with Args",
+					request = "launch",
+					program = "./${relativeFileDirname}",
+					outputMode = "remote",
+					args = function()
+						local args_string = vim.fn.input("Args: ")
+						return vim.split(args_string, " +")
+					end,
+				},
+				{
+					type = "delve",
+					name = "Debug Package (tests) with Args",
+					request = "launch",
+					mode = "test",
+					program = "./${relativeFileDirname}",
+					outputMode = "remote",
+					args = function()
+						local args_string = vim.fn.input("Test flags (e.g., -test.bench . -test.run TestName): ")
+						if args_string == "" then
+							return {}
+						end
+						local args = {}
+						for arg in args_string:gmatch("%S+") do
+							table.insert(args, arg)
+						end
+						return args
+					end,
+				},
+			}
+			require("mason-nvim-dap").default_setup(config)
+		end,
+	},
 })
